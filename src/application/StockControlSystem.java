1eff19f88df054a0fa702ccc7a706736982a5ef9
Hendrick Miqueias <hendrickmiqueias@hotmail.com>
2025-12-04 23:04:21 -0300

    Added functions: listStock, editStock and deleteStock

--- src/application/StockControlSystem.java
+++ src/application/StockControlSystem.java
@@ -60,8 +60,8 @@ public class StockControlSystem {
 		try (BufferedReader br = new BufferedReader(new FileReader(finalPath))) {
 			Type listType = new TypeToken<List<Stock>>() {
 			}.getType();
-			  List<Stock> loadedList = gson.fromJson(br, listType);
-		        return (loadedList != null) ? loadedList : new ArrayList<Stock>();
+			List<Stock> loadedList = gson.fromJson(br, listType);
+			return (loadedList != null) ? loadedList : new ArrayList<Stock>();
 		} catch (IOException e) {
 			try {
 				File file = new File(finalPath);
@@ -78,6 +78,8 @@ public class StockControlSystem {
 		System.out.println("-------------------------------- Stock Control System --------------------------------");
 		System.out.println("Type any of the current keys below to navigate in the system");
 		System.out.println();
+		// TO-DO add a product
+		// Out product
 		System.out.println("1 -  Search a product");
 		System.out.println("2 -  Create a product");
 		System.out.println("3 -  Delete a product");
@@ -89,7 +91,7 @@ public class StockControlSystem {
 		System.out.println("9 -  Delet a Stock");
 		int option = InputValidator.readInt(sc);
 
-		switch (option) // TO-DO adicionar todos os cases do menu aqui
+		switch (option) 
 		{
 		case 1:
 			searchProduct(sc);
@@ -102,25 +104,88 @@ public class StockControlSystem {
 		case 4:
 			editProduct(sc);
 			break;
+		case 5:
+			listStockProducts(sc);
+			break;
 		case 6:
 			stockList.add(createStock(sc));
+			break;
+		case 7:
+			listStocks(sc);
+			break;
+
+		case 8:
+			editStock(sc);
+			break;
+		case 9:
+			deleteStock(sc);
 			break;
 		}
 		saveStockList(System.getProperty("user.dir"));
 	}
-	private void editProduct(Scanner sc)
-	{
+
+	private void deleteStock(Scanner sc) {
+		Stock stock = selectStock(sc);
+		stockList.remove(stock);
+		System.out.println("Stock deleted successfully!");
+		menuEndPointReturn(sc);
+	}
+
+	/// List - Stocks
+	private void listStocks(Scanner sc) {
+		stockList.stream().map(s -> s.toString()).forEach(System.out::println);
+		menuEndPointReturn(sc);
+	}
+
+	/// List - Products
+	private void listStockProducts(Scanner sc) {
+		Stock stock = selectStock(sc);
+		stock.getProductsList().stream().map(p -> p.toString()).forEach(System.out::println);
+		menuEndPointReturn(sc);
+	}
+
+	/// Edit - Stock
+	private void editStock(Scanner sc) {
+		Stock stock = selectStock(sc);
+		System.out.println("Enter the field number you wish to edit: \n 0 = NAME ");
+		Integer choice = InputValidator.readInt(sc);
+		switch (choice) {
+		case 0:
+			System.out.println("Enter the new Stock name");
+			String newName = sc.nextLine();
+			stock.setStockName(newName);
+			System.out.println("Stock name changed successfully!");
+			menuEndPointReturn(sc);
+			break;
+		}
+	}
+
+	/// Edit - Product
+	private void editProduct(Scanner sc) {
 		Product product = selectProduct(sc);
 		System.out.println("Enter the field number you wish to edit: \n 0 = NAME \n 1 = PRICE \n 2 = QUANTITY");
 		Integer choice = InputValidator.readInt(sc);
-		switch(choice)
-		{
-		case 0:  System.out.println("Enter the New Product Name"); String productName = sc.nextLine(); product.setProductName(productName); break;
-		case 1:  System.out.println("Enter the New Product Price"); Double productPrice = InputValidator.readDouble(sc) ;product.setProductPrice(productPrice); break;
-		case 2:  System.out.println("Enter the New Product Quantity"); Integer productQuantity = InputValidator.readInt(sc); product.setProductQuantity(productQuantity); break;
+		switch (choice) {
+		case 0:
+			System.out.println("Enter the New Product Name");
+			String productName = sc.nextLine();
+			product.setProductName(productName);
+			break;
+		case 1:
+			System.out.println("Enter the New Product Price");
+			Double productPrice = InputValidator.readDouble(sc);
+			product.setProductPrice(productPrice);
+			break;
+		case 2:
+			System.out.println("Enter the New Product Quantity");
+			Integer productQuantity = InputValidator.readInt(sc);
+			product.setProductQuantity(productQuantity);
+			break;
 		}
 		menuEndPointReturn(sc);
 	}
+
+	/// Delete - Product
 	private void deleteProduct(Scanner sc) {
 		if (stockList != null) {
 			Stock stock = selectStock(sc);
@@ -139,12 +204,14 @@ public class StockControlSystem {
 		}
 	}
 
+	/// Return - Menu
 	private void menuEndPointReturn(Scanner sc) {
 		System.out.println("\nPress ENTER to go back to the main menu");
 		sc.nextLine();
 
 	}
 
+	/// Search - Product
 	private void searchProduct(Scanner sc) {
 		Product product = selectProduct(sc);
 		if (product != null) {
@@ -155,7 +222,7 @@ public class StockControlSystem {
 		}
 	}
 
-///Selects Product - Stock
+	/// Select - Stock
 	private Stock selectStock(Scanner sc) {
 		if (stockList != null) {
 
@@ -177,6 +244,7 @@ public class StockControlSystem {
 		}
 	}
 
+	/// Select - Product
 	private Product selectProduct(Scanner sc) {
 		if (this.stockList.size() > 0) {
 			Stock stock = selectStock(sc);
@@ -185,13 +253,12 @@ public class StockControlSystem {
 				System.out.println("Enter the product id: ");
 				Integer productId = InputValidator.readInt(sc);
 				for (Product product : stock.getProductsList()) {
-					while(!product.getProductId().equals(productId)) 
-					{
+					while (!product.getProductId().equals(productId)) {
 						System.out.println("This product Id dont exists!\n");
 						System.out.println("Enter the product id: ");
 						productId = InputValidator.readInt(sc);
 					}
-						return product;	
+					return product;
 				}
 			} else {
 				System.out.println("Dont have any product in this Stock yet");
@@ -206,6 +273,8 @@ public class StockControlSystem {
 		}
 		return null;
 	}
+
+	/// Create - Product
 	private void createProduct(Scanner sc) {
 		if (stockList != null) {
 
@@ -242,18 +311,16 @@ public class StockControlSystem {
 		}
 	}
 
+	/// Create - Stock
 	private Stock createStock(Scanner sc) {
 		System.out.println("Enter the Stock name:");
 		String stockName = sc.nextLine();
-		if(stockName.equals(""))
-		{
+		if (stockName.equals("")) {
 			System.out.println("The Stock name cannot be null!");
 			createStock(sc);
 			return null;
-		}
-		else
-		{
-			Stock createdStock = new Stock(stockName, stockList); 
+		} else {
+			Stock createdStock = new Stock(stockName, stockList);
 			System.out.println("Stock created successfully! \n" + createdStock.toString());
 			menuEndPointReturn(sc);
 			return createdStock;
@@ -273,5 +340,4 @@ public class StockControlSystem {
 		stock.removeProduct(product);
 	}
 
-	
 }
